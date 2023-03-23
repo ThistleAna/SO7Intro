f9368047794df6558fce158d407fd9b1856b7496
ANA KHASANAH <ana.khasanah@capita.com>
2023-03-23 13:27:41 +0000

    New cohort exercises

---
+++ src/Example1.java
@@ -0,0 +1,19 @@
+public class Example1 {
+    public static void main(String[] args) {
+        // starts your code from here
+        double height = 13.5; // double datatype = contains fraction number
+        double width = 25.3; // name of variable is width, contains value of 5
+
+        // calculate perimeter
+        double perimeter = 2 * (width + height);
+        // calculate area of rectangle
+
+        // print the information
+        System.out.println("== RECTANGLE CALCULATOR PROGRAM==");
+        System.out.println("Rectangle height = " + height);
+        System.out.println("Rectangle width = " + width);
+        System.out.println("Calculating .....");
+        System.out.println("Perimeter = " + perimeter);
+        // print the area of the rectangle
+    }
+}
