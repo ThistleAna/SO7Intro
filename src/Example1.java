b5f42b87d5cf5e5f4a2f274fa360ca6e576c4011
ANA KHASANAH <ana.khasanah@capita.com>
2023-03-28 13:02:44 +0100

    Added Circle Calculator, Java Input exercise, modify Example 1 to accept user inputs width and height

--- src/Example1.java
+++ src/Example1.java
@@ -1,19 +1,26 @@
+import java.util.Scanner;
 public class Example1 {
     public static void main(String[] args) {
-        // starts your code from here
-        double height = 13.5; // double datatype = contains fraction number
-        double width = 25.3; // name of variable is width, contains value of 5
+        // create Scanner object
+        Scanner reader = new Scanner(System.in);
+
+        // ask user to enter the height
+        System.out.println("== RECTANGLE CALCULATOR PROGRAM==");
+        System.out.println("Enter height : ");
+        double height = reader.nextDouble(); // double datatype = contains fraction number
+        System.out.println("Enter width : ");
+        double width = reader.nextDouble(); // name of variable is width, contains value of 5
 
         // calculate perimeter
         double perimeter = 2 * (width + height);
         // calculate area of rectangle
-
+        double area = width * height;
         // print the information
-        System.out.println("== RECTANGLE CALCULATOR PROGRAM==");
+
         System.out.println("Rectangle height = " + height);
         System.out.println("Rectangle width = " + width);
         System.out.println("Calculating .....");
         System.out.println("Perimeter = " + perimeter);
-        // print the area of the rectangle
+        System.out.println("Area = " + area);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,8 @@
 public class Main { //this is the main class
     public static void main(String[] args) { // shortcut is main, main method
         // starts your code from here
-        System.out.println("Hello, I am learning Java right now!"); // shortcut is sout
-        System.out.println("     *****");
-        System.out.println("     *****");
-        System.out.println("     *****");
-        System.out.println("     *****");
-        System.out.println("     *****");
-        System.out.println("End of program");
+        System.out.print("Hello, I am learning Java right now!"); // shortcut is sout
+        System.out.print("\nHello, I am learning Java right now!"); // shortcut is sout
     } // end of main method
 } // end of main class
 
