e3ecd4c037890e74d5220eb8c47af17e99597131
ANA KHASANAH <ana.khasanah@capita.com>
2023-03-28 13:28:46 +0100

    Added Circle Calculator, Java Input exercise, modify Example 1 to accept user inputs width and height

--- src/CircleCalculator.java
+++ src/CircleCalculator.java
@@ -1,8 +1,12 @@
+// import java Scanner
+import java.util.Scanner;
 public class CircleCalculator {
     public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
         // write your codes here
         // declare a double variable to store the circle radius
-        double circleRadius = 12.0;
+        System.out.println("Enter the circle radius : ");
+        double circleRadius = reader.nextDouble();
         // calculate diameter of the circle
         double circleDiameter = 2 * circleRadius;
         // calculate circumference of the circle
